f09bb7c95a16684e4e2ac1f2f14321ed3449bf01
Muyon Rivera, Christian Alfredo <christian.muyon.rivera@gmail.com>
2022-01-10 14:22:03 -0500

    Implementacion de descarga de archivo csv en endpoint obtenerReporteTransaccionesCliente

--- src/main/java/com/pichincha/inventario/controller/ReporteController.java
+++ src/main/java/com/pichincha/inventario/controller/ReporteController.java
@@ -3,10 +3,17 @@
  */
 package com.pichincha.inventario.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -44,13 +51,46 @@ public class ReporteController {
 
 	@GetMapping("/obtenerReporteTransaccionesCliente/{codigoCliente}/{fechaInicio}/{fechaFin}")
 	public ResponseEntity<?> obtenerReporteTransaccionesCliente(@PathVariable("codigoCliente") Long codigoCliente,
-			@PathVariable("fechaInicio") String fechaInicio, @PathVariable("fechaFin") String fechaFin) {
+			@PathVariable("fechaInicio") String fechaInicio, @PathVariable("fechaFin") String fechaFin)
+			throws IOException {
+		FileWriter fileWriter = null;
 		try {
 			List<ReporteTransaccionesClienteDTO> resultado = reporteServicio
 					.obtenerReporteTransaccionesCliente(codigoCliente, fechaInicio, fechaFin);
-			return new ResponseEntity<>(resultado, HttpStatus.OK);
-		} catch (InventarioException e) {
+
+			/* Construccion de contenido de archivo */
+			StringBuilder fileContent = new StringBuilder(
+					"ID,TRANSACCION,FECHA_TRANSACCION,CLIENTE_IDENTIFICACION,CLIENTE_NOMBRE\n");
+			for (ReporteTransaccionesClienteDTO reporteTransaccionesClienteDTO : resultado) {
+				fileContent.append(reporteTransaccionesClienteDTO.getId()).append(",")
+						.append(reporteTransaccionesClienteDTO.getTransaccion()).append(",")
+						.append(reporteTransaccionesClienteDTO.getFechaTransaccion()).append(",")
+						.append(reporteTransaccionesClienteDTO.getClienteIdentificacion()).append(",")
+						.append(reporteTransaccionesClienteDTO.getClienteNombre()).append("\n");
+			}
+
+			String fileName = "reporteTransaccionesCliente.csv";
+			fileWriter = new FileWriter(fileName);
+			fileWriter.write(fileContent.toString());
+			fileWriter.flush();
+
+			File file = new File(fileName);
+
+			InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+			HttpHeaders headers = new HttpHeaders();
+			headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getName()));
+			headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+			headers.add("Pragma", "no-cache");
+			headers.add("Expires", "0");
+
+			return ResponseEntity.ok().headers(headers).contentLength(file.length())
+					.contentType(MediaType.parseMediaType("application/txt")).body(resource);
+		} catch (InventarioException | IOException e) {
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		} finally {
+			if (fileWriter != null) {
+				fileWriter.close();
+			}
 		}
 
 	}
